05fb5615874ac417745928f10a4086af9cd02ee6
Mr_Jing <admin@imojie.com>
2016-09-11 21:18:30 +0800

    安装 laravel-ide-helper

--- config/app.php
+++ config/app.php
@@ -138,7 +138,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         Barryvdh\Debugbar\ServiceProvider::class,
-
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         /*
          * Application Service Providers...
          */
