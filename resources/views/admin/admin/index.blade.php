875ed74844d33979f23ea63d48b8a4641b9a3eac
Mr_Jing <admin@imojie.com>
2016-09-28 17:04:48 +0800

    layout.footer

--- resources/views/admin/admin/index.blade.php
+++ resources/views/admin/admin/index.blade.php
@@ -914,13 +914,7 @@
     </div>
     <!-- /.content-wrapper -->
 
-    <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-            <b>Version</b> 2.3.6
-        </div>
-        <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
-        reserved.
-    </footer>
+    @include('admin.layout.footer')
 
     <!-- Control Sidebar -->
     <aside class="control-sidebar control-sidebar-dark">
