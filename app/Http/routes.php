804b07db866f59d74013afd1b9a31b5efb277c3f
Mr_Jing <admin@imojie.com>
2016-09-28 18:11:55 +0800

    添加控制器

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,4 +8,13 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
     // 后台入口
     Route::get('/', ['uses' => 'AdminController@index']);
+
+    // 用户角色
+    Route::resource('roles', 'RoleController', ['except' => ['show']]);
+
+    // 访问权限
+    Route::resource('routes', 'RouteController', ['except' => ['show']]);
+
+    // 菜单显示
+    Route::resource('menus', 'MenuController', ['except' => ['show']]);
 });
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -25,16 +25,16 @@
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="active treeview">
+            <li class="treeview">
                 <a href="#">
-                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-                    <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+                    <i class="fa fa-dashboard"></i>
+                    <span>权限管理</span>
+                    <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-                    <li class="active"><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+                    <li><a href="menus"><i class="fa fa-circle-o"></i>菜单管理</a></li>
+                    <li><a href="routes"><i class="fa fa-circle-o"></i>访问控制</a></li>
+                    <li><a href="roles"><i class="fa fa-circle-o"></i>角色管理</a></li>
                 </ul>
             </li>
             <li class="treeview">
