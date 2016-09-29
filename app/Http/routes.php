abbfce1e617e8febe29e6282f14f63567872771f
Mr_Jing <admin@imojie.com>
2016-09-29 09:49:47 +0800

    修改细节

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,12 +9,12 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
     // 后台入口
     Route::get('/', ['uses' => 'AdminController@index']);
 
-    // 用户角色
-    Route::resource('roles', 'RoleController', ['except' => ['show']]);
+    // 菜单显示
+    Route::resource('menus', 'MenuController', ['except' => ['show']]);
 
     // 访问权限
     Route::resource('routes', 'RouteController', ['except' => ['show']]);
 
-    // 菜单显示
-    Route::resource('menus', 'MenuController', ['except' => ['show']]);
+    // 用户角色
+    Route::resource('roles', 'RoleController', ['except' => ['show']]);
 });
