937e886af5fb96d99194ee1b982f75283446df38
Mr_Jing <admin@imojie.com>
2016-10-10 09:44:24 +0800

    修改细节

--- resources/views/admin/menu/index.blade.php
+++ resources/views/admin/menu/index.blade.php
@@ -22,37 +22,17 @@
                 <div class="col-xs-12">
                     <div class="box">
                         <div class="box-header">
-                            <h3 class="box-title">菜单列表</h3>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <a href="{{url('admin/menus/create')}}" class="btn btn-block btn-success"
+                                       role="button">添加菜单</a>
+                                </div>
+                                <div class="col-sm-10"></div>
+                            </div>
                         </div>
                         <!-- /.box-header -->
                         <div class="box-body">
                             <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
-                                <div class="row" style="margin-bottom: 15px;">
-                                    <div class="col-sm-2">
-                                        <a href="{{url('admin/menus/create')}}" class="btn btn-block btn-success"
-                                           role="button">添加菜单</a>
-                                    </div>
-                                    <div class="col-sm-10"></div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <div class="dataTables_length" id="example1_length"><label>Show <select
-                                                        name="example1_length" aria-controls="example1"
-                                                        class="form-control input-sm">
-                                                    <option value="10">10</option>
-                                                    <option value="25">25</option>
-                                                    <option value="50">50</option>
-                                                    <option value="100">100</option>
-                                                </select> entries</label></div>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <div id="example1_filter" class="dataTables_filter"><label>Search:<input
-                                                        type="search" class="form-control input-sm" placeholder=""
-                                                        aria-controls="example1"></label></div>
-                                    </div>
-                                </div>
-
                                 <div class="row">
                                     <div class="col-sm-12">
                                         <table id="example1" class="table table-bordered table-striped dataTable"
@@ -113,40 +93,6 @@
                                             @endforeach
                                             </tbody>
                                         </table>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-5">
-                                        <div class="dataTables_info" id="example1_info" role="status"
-                                             aria-live="polite">Showing 1 to 10 of 57 entries
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-7">
-                                        <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
-                                            <ul class="pagination">
-                                                <li class="paginate_button previous disabled" id="example1_previous"><a
-                                                            href="#" aria-controls="example1" data-dt-idx="0"
-                                                            tabindex="0">Previous</a></li>
-                                                <li class="paginate_button active"><a href="#" aria-controls="example1"
-                                                                                      data-dt-idx="1" tabindex="0">1</a>
-                                                </li>
-                                                <li class="paginate_button "><a href="#" aria-controls="example1"
-                                                                                data-dt-idx="2" tabindex="0">2</a></li>
-                                                <li class="paginate_button "><a href="#" aria-controls="example1"
-                                                                                data-dt-idx="3" tabindex="0">3</a></li>
-                                                <li class="paginate_button "><a href="#" aria-controls="example1"
-                                                                                data-dt-idx="4" tabindex="0">4</a></li>
-                                                <li class="paginate_button "><a href="#" aria-controls="example1"
-                                                                                data-dt-idx="5" tabindex="0">5</a></li>
-                                                <li class="paginate_button "><a href="#" aria-controls="example1"
-                                                                                data-dt-idx="6" tabindex="0">6</a></li>
-                                                <li class="paginate_button next" id="example1_next"><a href="#"
-                                                                                                       aria-controls="example1"
-                                                                                                       data-dt-idx="7"
-                                                                                                       tabindex="0">Next</a>
-                                                </li>
-                                            </ul>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
